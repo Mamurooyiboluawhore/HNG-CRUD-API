ae83b7e61d582f610e34c6912f6282d82bab665c
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-09-16 15:25:55 +0100

    Update db.py

--- db.py
+++ db.py
@@ -6,5 +6,5 @@ db = SQLAlchemy()
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    email = db.Column(db.String(75), nullable=False)
+   # age = db.Column(db.Integer, nullable=False)
+    #email = db.Column(db.String(75), nullable=False)
